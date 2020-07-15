3469496c75933b60e77339913878dafae3f8f881
Noel Dawe <noel@slice.is>
2020-07-15 15:17:38 -0400

    prune setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,4 @@
 #!/usr/bin/env python
-"""
-Simple check list from AllenNLP repo: https://github.com/allenai/allennlp/blob/master/setup.py
-
-To create the package for pypi.
-
-1. Change the version in __init__.py and setup.py.
-
-2. Commit these changes with the message: "Release: VERSION"
-
-3. Add a tag in git to mark the release: "git tag VERSION -m'Adds tag VERSION for pypi' "
-   Push the tag to git: git push --tags origin master
-
-4. Build both the sources and the wheel. Do not change anything in setup.py between
-   creating the wheel and the source distribution (obviously).
-
-   For the wheel, run: "python setup.py bdist_wheel" in the top level allennlp directory.
-   (this will build a wheel for the python version you use to build it - make sure you use python 3.x).
-
-   For the sources, run: "python setup.py sdist"
-   You should now have a /dist directory with both .whl and .tar.gz source versions of allennlp.
-
-5. Check that everything looks correct by uploading the package to the pypi test server:
-
-   twine upload dist/* -r pypitest
-   (pypi suggest using twine as other methods upload files via plaintext.)
-
-   Check that you can install it in a virtualenv by running:
-   pip install -i https://testpypi.python.org/pypi neuralcoref
-
-6. Upload the final version to actual pypi:
-   twine upload dist/* -r pypi
-
-7. Copy the release notes from RELEASE.md to the tag in github once everything is looking hunky-dory.
-
-"""
 import os
 import subprocess
 import sys
@@ -126,22 +91,6 @@ class build_ext_subclass(build_ext, build_ext_options):
         build_ext.build_extensions(self)
 
 
-# def is_installed(requirement):
-#     try:
-#         pkg_resources.require(requirement)
-#     except pkg_resources.ResolutionError:
-#         return False
-#     else:
-#         return True
-
-# if not is_installed('numpy>=1.11.0') or not is_installed('spacy>=2.1.0'):
-#     print(textwrap.dedent("""
-#             Error: requirements needs to be installed first.
-#             You can install them via:
-#             $ pip install -r requirements.txt
-#             """), file=sys.stderr)
-#     exit(1)
-
 @contextlib.contextmanager
 def chdir(new_dir):
     old_dir = os.getcwd()
@@ -163,15 +112,11 @@ def generate_cython(root, source):
         raise RuntimeError('Running cythonize failed')
 
 
-def is_source_release(path):
-    return os.path.exists(os.path.join(path, 'PKG-INFO'))
-
-
 def setup_package():
     root = os.path.abspath(os.path.dirname(__file__))
     with chdir(root):
-        if not is_source_release(root):
-            generate_cython(root, 'neuralcoref')
+
+        generate_cython(root, 'neuralcoref')
 
         include_dirs = [
             get_python_inc(plat_specific=True),
